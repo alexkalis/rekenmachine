4daadf062734ba523338f4ba9a24e6dc60533a0f
alexkalis <99039736@mydavinci.nl>
2017-01-16 09:11:03 +0100

    Add files via upload

---
+++ calc2.js.js
@@ -0,0 +1,51 @@
+"use strict";
+
+var input = document.getElementById('input'),
+	number  = document.querySelectorAll('.numbers div'), 
+	operator = 
+	document.querySelectorAll('.operators div'),
+	result = document.getElementById('result'),
+	clear = document.getElementById('clear'),
+	resultDisplayed = false;
+
+	for (var i = 0; i < number.length; i++) {
+		number[i].addEventListener("click", function(e) {
+
+
+	var currentString = input.innerHTML;
+	var lastChar = currentString[currentString.length -1];
+	if (resultDisplayed === false) {
+		input.innerHTML += e.target.innerHTML;
+	} else if (resultDisplayed === true &&
+		lastChar === "+" || lastChar === "-" ||
+		lastChar === "×" || lastChar === "÷" ) {
+
+		resultDisplayed = false;
+		input.innerHTML += e.target.innerHTML;
+	} else {
+		resultDisplayed = false;
+		input.innerHTML = "";
+		input.innerHTML += e.target.innerHTML;
+	}	
+} );
+
+
+for (var  i = 0; i < operator.length; i++) {
+	operator[i].addEventListener("click" function(e) {
+
+		var currentString = input.innerHTML;
+		var lastChar = 	currentString[currentString.length - 1];
+
+		if (lastChar === "+" || lastChar === "-" ||
+			lastChar === "×" || lastChar === "÷") {
+			var newString = currentString.substring(0, currentString.length -1) + e.target.innerHTML;
+		input.innerHTML = newString;
+		} else if (currentString.length == 0) {
+			console.log("enter a number first");
+
+		} else {
+			input.innerHTML += e.target.innerHTML;
+
+		}
+	});
+}
